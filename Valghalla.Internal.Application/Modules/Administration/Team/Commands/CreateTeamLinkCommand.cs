ff4283d26d577a4f8b233f9012f9c999a10c3edc
levanduc09 <levanduc094@gmail.com>
2024-03-21 10:13:19 +0700

    fix: add external domain to team link

--- Valghalla.Internal.Application/Modules/Administration/Team/Commands/CreateTeamLinkCommand.cs
+++ Valghalla.Internal.Application/Modules/Administration/Team/Commands/CreateTeamLinkCommand.cs
@@ -1,5 +1,6 @@
 ﻿using FluentValidation;
 using Valghalla.Application.Abstractions.Messaging;
+using Valghalla.Application.Tenant;
 using Valghalla.Internal.Application.Modules.Administration.Team.Interfaces;
 
 namespace Valghalla.Internal.Application.Modules.Team.Commands
@@ -21,16 +22,18 @@ namespace Valghalla.Internal.Application.Modules.Team.Commands
     internal class CreateTeamLinkCommandHandler : ICommandHandler<CreateTeamLinkCommand, Response<string>>
     {
         private readonly ITeamCommandRepository teamCommandRepository;
-        public CreateTeamLinkCommandHandler(ITeamCommandRepository teamCommandRepository)
+        private readonly ITenantContextProvider tenantContextProvider;
+        public CreateTeamLinkCommandHandler(ITeamCommandRepository teamCommandRepository, ITenantContextProvider tenantContextProvider)
         {
             this.teamCommandRepository = teamCommandRepository;
+            this.tenantContextProvider = tenantContextProvider;
         }
 
         public async Task<Response<string>> Handle(CreateTeamLinkCommand command, CancellationToken cancellationToken)
         {
             string hashValue = await teamCommandRepository.CreateTeamLinkAsync(command, cancellationToken);
 
-            return Response.Ok("/team-link?id=" + hashValue);
+            return Response.Ok($"{tenantContextProvider.CurrentTenant.ExternalDomain}/team-link?id=" + hashValue);
         }
     }
 }
